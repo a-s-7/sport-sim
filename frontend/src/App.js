dbc6749526ed67436ea086c34a5a016f93c4ca21
Ayush Saldhi <99509922+a-s-7@users.noreply.github.com>
2024-08-11 20:00:56 -0700

    Extracted components, started page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,12 @@
 import './App.css';
 import NavBar from "./components/NavBar";
-import React, {useState, useEffect} from "react";
-import WTCControlBar from "./components/WTCControlBar";
-import MatchCardPanel from "./components/MatchCardPanel";
-import WTCPointsTable from "./components/WTCPointsTable";
-
+import WorldTestChampionship from "./pages/WorldTestChampionship";
 
 function App() {
-    const [selectedTeams, setSelectedTeams] = useState([]);
-    const [data, setData] = useState([]);
-
-    const [matchAreaKey, setMatchAreaKey] = useState(0);
-    const [pointsTableKey, setPointsTableKey] = useState(0);
-
-    const refreshPointsTable = () => {
-        setPointsTableKey(pointsTableKey + 1);
-    }
-
-    const refreshMatchArea = () => {
-        setMatchAreaKey(matchAreaKey + 1);
-    }
-
-    const handleRefresh = async () => {
-        await fetchData();
-        refreshMatchArea();
-        refreshPointsTable();
-    }
-
-    const fetchData = async () => {
-        let url = `http://127.0.0.1:5000/WTC/matches/All`;
-
-        if (selectedTeams.length > 0) {
-            let teamNames = selectedTeams.map(team => team.label).join("-");
-            url = `http://127.0.0.1:5000/WTC/matches/${teamNames}`;
-        }
-
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error("Response was not ok");
-            }
-            const result = await response.json();
-            setData(result);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, [selectedTeams]);
-
-
     return (
         <div className="App">
             <NavBar></NavBar>
-            <WTCControlBar refFunc={handleRefresh}
-                           matchCount={Array.isArray(data[2]) ? data[2].length : 0}
-                           teams={selectedTeams}
-                           sst={setSelectedTeams}>
-            </WTCControlBar>
-
-            <div className="matchArea">
-                <div className="matchCardContainer">
-                    <MatchCardPanel key={matchAreaKey}
-                                    onMatchUpdate={refreshPointsTable}
-                                    matches={data}/>
-                </div>
-                <div className="tableContainer">
-                    <WTCPointsTable key={pointsTableKey}/>
-                </div>
-            </div>
+            <WorldTestChampionship/>
         </div>
     );
 }
---
+++ frontend/src/components/WTCMatchArea.js
@@ -0,0 +1,18 @@
+import React from "react";
+import MatchCardPanel from "./MatchCardPanel";
+import WTCPointsTable from "./WTCPointsTable";
+
+function WTCMatchArea({pTableKey, rPointsTable, data}) {
+    return (
+        <div className="matchArea">
+            <div className="matchCardContainer">
+                <MatchCardPanel onMatchUpdate={rPointsTable} matches={data}/>
+            </div>
+            <div className="tableContainer">
+                <WTCPointsTable key={pTableKey}/>
+            </div>
+        </div>
+    );
+}
+
+export default WTCMatchArea;

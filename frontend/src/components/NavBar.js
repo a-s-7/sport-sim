58d9cc1835f8456b37a1f411ada056ec91da8df5
Ayush Saldhi <99509922+a-s-7@users.noreply.github.com>
2024-08-14 20:59:49 -0700

    Add button url path coloring

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,30 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 
 function NavBar() {
+    const location = useLocation();
+    const path = location.pathname;
+
     return (
         <div className="navContainer">
             <div className="logoArea">
                 <NavLink to="/">SPORT SIM</NavLink>
             </div>
             <div className="appModeArea">
-                <NavLink to="/wtc">ICC WTC</NavLink>
-                <NavLink to="/ipl">IPL</NavLink>
+                <NavLink to="/wtc"
+                         style={{
+                             background: path === "/wtc" ? "black" : "white",
+                             color: path === "/wtc" ? "white" : "black"
+                         }}>
+                    ICC WTC
+                </NavLink>
+                <NavLink to="/ipl"
+                          style={{
+                             background: path === "/ipl" ? "black" : "white",
+                             color: path === "/ipl" ? "white" : "black"
+                         }}>
+                    IPL
+                </NavLink>
             </div>
             <div className="userArea">
                 USER

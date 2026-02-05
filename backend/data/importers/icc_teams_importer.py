661829ac046313211d88646c4bb419966ab16cb9
Ayush Saldhi <99509922+a-s-7@users.noreply.github.com>
2026-02-04 23:56:43 -0800

    feat: Implement ICC team data import, define structured T20 World Cup event data, and add environment files to gitignore.

---
+++ backend/data/importers/icc_teams_importer.py
@@ -0,0 +1,43 @@
+import json
+import os
+from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
+
+########### ADD ICC TEAMS (TEAMS COLLECTION) ###########
+
+if os.getenv("RENDER_STATUS") != "TRUE":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+connection_string = os.getenv('MONGODB_URI')
+
+if not connection_string:
+    raise ValueError("MONGODB_URI not found in environment variables")
+
+client = MongoClient(connection_string)
+db = client['events']
+
+teams_collection = db['teams']
+teams_collection.create_index("acronym", unique=True)
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_path = os.path.join(base_dir, "sources", "events", "icc-teams.json")
+
+with open(file_path, 'r') as file:
+    json_info = json.load(file)
+
+try:
+    result = teams_collection.insert_many(json_info["teams"], ordered=False)
+    print("Inserted IDs:", result.inserted_ids)
+except BulkWriteError as e:
+    write_errors = e.details.get('writeErrors', [])
+
+    failed_acronyms = []
+
+    for err in write_errors:
+        team = json_info["teams"][err['index']]
+        failed_acronyms.append(team['acronym'])
+
+    inserted_count = len(json_info["teams"]) - len(failed_acronyms)
+    print(f"Inserted {inserted_count} teams")
+    print("Skipped duplicates:", failed_acronyms)
